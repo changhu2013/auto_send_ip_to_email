83082cbd1318f39a5eb88a6e72ee33774cef5df1
changhu <o52tiger@gmail.com>
2016-10-21 22:31:44 +0800

    init

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport('smtps://o52tiger%40163.com:Cp1y01m04D@smtp.163.com');
 
-var ip_list = require('./ip_list');
-var text = "TIME:" + new Date() + " \nIP:\n" + ip_list.join(' \n');
+function send_email(){
+    var ip_list = require('./ip_list');
+    var text = "TIME:" + new Date() + " \nIP:\n" + ip_list.join(' \n');
 
-var mailOptions = {
-    from: 'o52tiger@163.com',
-    to: 'o52tiger@qq.com',
-    subject: '树莓派IP地址',
-    text: text
-};
+    var mailOptions = {
+        from: 'o52tiger@163.com',
+        to: 'o52tiger@qq.com',
+        subject: '树莓派IP地址',
+        text: text
+    };
 
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-});
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+}
+
+setTimeout(send_email, 10000);
