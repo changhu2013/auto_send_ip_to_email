45416c36828088c2c915f12ec6e7b4ae9115199c
changhu <o52tiger@gmail.com>
2016-10-22 10:23:24 +0800

    -

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
+var os=require('os');
 var nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport('smtps://o52tiger%40163.com:Cp1y01m04D@smtp.163.com');
+var TIMEOUT = 10000;
+
+function get_ip_list(){
+    var ifaces = os.networkInterfaces();
+    var ip_list = [];
+
+    for (var dev in ifaces) {
+        var alias = 0;
+        ifaces[dev].forEach(function(details){
+            if (details.family == 'IPv4') {
+            ip_list.push(dev + (alias ? ':' + alias : '') + ' ' + details.address);
+            ++alias;
+            }
+        });
+    }
+
+    return ip_list;
+}
 
 function send_email(){
-    var ip_list = require('./ip_list');
-    var text = "TIME:" + new Date() + " \nIP:\n" + ip_list.join(' \n');
+    var transporter = nodemailer.createTransport('smtps://o52tiger%40163.com:Cp1y01m04D@smtp.163.com');
+    var ip_list = get_ip_list();
+    var text = "TIME : \n"
+            + new Date() + " \n"
+            + "IP   :\n"
+            + ip_list.join(' \n');
 
     var mailOptions = {
-        from: 'o52tiger@163.com',
-        to: 'o52tiger@qq.com',
-        subject: '树莓派IP地址',
-        text: text
+        from    : 'o52tiger@163.com',
+        to      : 'o52tiger@qq.com',
+        subject : '树莓派IP地址',
+        text    : text
     };
 
     transporter.sendMail(mailOptions, function(error, info){
@@ -20,4 +42,4 @@ function send_email(){
     });
 }
 
-setTimeout(send_email, 10000);
+setTimeout(send_email, TIMEOUT);
